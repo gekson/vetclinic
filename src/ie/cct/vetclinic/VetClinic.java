c7659e5aa75289bbf5e58e247c132870d60e7180
Gekson Cinaty <gekson.cinaty@indt.org.br>
2020-04-09 03:23:07 -0300

    MENU - BASIC FUNCTIONALITY

--- src/ie/cct/vetclinic/VetClinic.java
+++ src/ie/cct/vetclinic/VetClinic.java
@@ -12,6 +12,8 @@ import ie.cct.vetclinic.staff.medical.TraineeVet;
 import ie.cct.vetclinic.staff.medical.Veterinarian;
 import ie.cct.vetclinic.util.Generator;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -31,6 +33,70 @@ public class VetClinic {
     private static int idStaff = 1;
     private static Map<Integer, List<Animal>> treatmentMap;
 
+    public VetClinic() {
+        int choice = 0;
+
+        do {
+            choice = getUserOption();
+
+            //decide which operation to use - call a method to do the work each time
+
+            if (choice == 1) {
+                listAllStaff();
+            }
+            else if ( choice == 2) {
+                int userNum = 0;
+                do {
+                    userNum = getUserInt("\tPlease enter a number: \n\t1: - list all Nurses" +
+                            "\n\t2: - list all TraineeVet\n\t3: - list all Veterinarians\n\t4: - Return");
+                    switch (userNum) {
+                        case 1 :
+                            listStaffByCategorie("Nurse");
+                            break;
+                        case 2 :
+                            listStaffByCategorie("TraineeVet");
+                            break;
+                        case 3 :
+                            listStaffByCategorie("Veterinarian");
+                            break;
+                        default :
+                            System.out.println("Enter ONLY numbers between 1 and 3!");
+                            break;
+                    }
+                } while (userNum != 4);
+            }
+            else if (choice == 3) {
+
+                System.out.println("choice 3");
+            }
+            else if (choice == 4) {
+
+                int userNum = getUserInt("Please enter a number, 1 for Nurse or 2 for TraineeVet or 3 for Veterinarian.");
+
+                System.out.println(userNum);
+            }
+            else if (choice == 5) {
+
+                System.out.println("choice 3");
+            }
+            else if (choice == 6) {
+
+                System.out.println("choice 3");
+            }
+            else if (choice == 7) {
+
+                System.out.println("choice 3");
+            }
+            else {
+                //choice must be 8. nothing to do here
+            }
+
+        } while (choice != 8);
+
+        System.out.println("Thank you for using this program. Please make cheques payable to Ken Healy.");
+
+    }
+
     public static void main(String[] args) {
         System.out.println("*************** Vet Clicnic Application Starts Here ***************");
         listAdminStaff = new ArrayList<>();
@@ -44,7 +110,6 @@ public class VetClinic {
 
         createAdminStaff();
 
-        System.out.println("*************** List Admin Staff ***************");
 //        listAdminStaff.stream().forEach(e -> System.out.println(e.toString()));
 
 //        System.out.println("---------------");
@@ -58,18 +123,16 @@ public class VetClinic {
 //        listMedicalStaff.stream().forEach(e -> System.out.println(e.toString()));
 
 
-        System.out.println("---------------");
         createAnimals();
 
         assignAnimal();
+
+        new VetClinic();
 
 
 //        System.out.println("*************** List Animals ***************");
 //        listAnimal.stream().forEach(e -> System.out.println(e.toString()));
 
-        //Put in menu
-        listAllStaff();
-        listStaffByCategorie("Nurse");
     }
 
     private static void createAdminStaff() {
@@ -136,7 +199,6 @@ public class VetClinic {
     }
 
     private static void assignAnimal(){
-        Random generator = new Random();
         for (Animal animal: listAnimal) {
             int rnd = ThreadLocalRandom.current().nextInt(11, 41);
             if(treatmentMap.containsKey(rnd)) {
@@ -147,6 +209,84 @@ public class VetClinic {
             listAnimalTreatment.add(animal);
             treatmentMap.put(rnd, listAnimalTreatment);
         }
+    }
+
+    /**
+     * Menu
+     */
+    public int getUserOption() {
+        // display menu and return a VALID choice.
+        // keep asking until valid choice made
+
+        BufferedReader myReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input;
+
+        do {
+            //display menu
+            System.out.println("\n\nPlease choose an option from the list below.\n\n");	// the \n create a blank line. this is just to space the output a bit
+
+            System.out.println("1: - List all staff.");
+            System.out.println("2: - List staff by categories (e.g. list all Nurses).");
+            System.out.println("3: - List all Admin staff performing a certain task.");
+            System.out.println("4: - Search for a specific member of staff by name.");
+            System.out.println("5: - List all animals.");
+            System.out.println("6: - List animals by various types (e.g. list all dogs.");
+            System.out.println("7: - Search for a specific animal by name.");
+            System.out.println("8: Exit.");
+
+            try {
+
+                input = Integer.parseInt(myReader.readLine()); //convert to an int - will trigger 'catch' if user enters non-integer
+
+                if ((input<1) || (input>8)) {
+                    //user selection invalid
+                    System.out.println("Please enter a number 1-8 corresponding to your choice");
+                }
+
+            }catch (Exception e) {
+                //user did not enter a number at all
+                System.out.println("Please enter a number 1-8 corresponding to your choice");
+                input = 0; //ensures loop keeps going
+            }
+
+        }while ((input<1) || (input > 8));
+
+        //user selection MUST be between 1 and 5
+
+        return input;
+    }
+
+    public int getUserInt(String myPrompt) {
+        //prompt user with message myPrompt
+        //return a number input from user
+
+        BufferedReader myInput = new BufferedReader(new InputStreamReader (System.in));
+        int userInput = 0;
+        boolean valid = false;
+
+        do {
+            try {
+                //using try-catch for user input
+                //in case there is an error
+
+                //prompt user & get input
+                System.out.println(myPrompt);
+                userInput = Integer.parseInt(myInput.readLine());
+                valid = true; //must be valid - otherwise the 'catch' would have been triggered
+
+            }
+            catch( Exception e) {
+                //because it is possible that there will be an error, must return something here
+                //just an empty string
+                System.out.println("That was not a number! Please enter an integer!");
+                valid = false;
+            }
+
+        }while (!valid); //keep going until an integer is entered
+
+        //we have an integer value - return it
+        return userInput;
     }
 
     /**
@@ -167,7 +307,7 @@ public class VetClinic {
      * @param categorie
      */
     private static void listStaffByCategorie(String categorie){
-
+        System.out.println("*************** List "+categorie+" Staff ***************");
         List<Staff> listFinal = new ArrayList<>();
         for (Staff staff: listAllStaff) {
             if(staff.getClass().getSimpleName().equals(categorie)){
